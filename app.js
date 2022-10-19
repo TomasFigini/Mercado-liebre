a1cc44fd0248690de3edc9917c3e0e553b0605c9
TomasFigini <tomasfigini79@gmail.com>
2022-10-19 19:00:57 -0300

    agregue cambios a package.json y app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express();
 const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
-    console.log('Server corriendo en el puerto 3030');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Server corriendo en el puerto 3000');
 });
 
 app.get('/', (req,res)=>{
